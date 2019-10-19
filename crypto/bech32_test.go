2803dac25b523cdc75773517b304eaab6278c003
Seongjae Choi <sjchoi@kodebox.io>
2019-10-19 13:28:18 +0900

    Implement bech32 encoding and decoding
    All functions of bech32 are not exposed to the user.

---
+++ crypto/bech32_test.go
@@ -0,0 +1,20 @@
+package crypto
+
+import (
+	"bytes"
+	"encoding/hex"
+	"testing"
+)
+
+func TestBech32(t *testing.T) {
+	hex, _ := hex.DecodeString("aabbccddeeff012a3b4d5e6f9922")
+	r := bech32Encode("wcc", toWords(hex))
+	if r != "wcc42aueh0wluqj5w6dtehejgsv7dgs8" {
+		t.Fatal("Bech32 Encode Error")
+	}
+
+	dec := fromWords(bech32Decode(r, "wcc"))
+	if bytes.Compare(dec, hex) != 0 {
+		t.Fatal("Bech32 Decode Error")
+	}
+}

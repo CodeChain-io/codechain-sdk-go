4ca682c7c157149fa3aa5d3b3834ff6693f99249
Park Juhyung <jh@codechain.io>
2019-10-25 12:15:14 +0900

    Fix lint errors

--- primitives/bignumber.go
+++ primitives/bignumber.go
@@ -2,8 +2,9 @@ package primitives
 
 import (
 	"encoding/hex"
-	"github.com/ethereum/go-ethereum/rlp"
 	"math/big"
+
+	"github.com/ethereum/go-ethereum/rlp"
 )
 
 type U64 big.Int
@@ -21,59 +22,59 @@ func NewU64(v string) U64 {
 	return U64(x)
 }
 
-func (z *U64) set(y big.Int) {
-	*z = U64(y)
+func (u *U64) set(y big.Int) {
+	*u = U64(y)
 }
 
-func (z *U64) Add(x *U64, y *U64) *U64 {
+func (u *U64) Add(x *U64, y *U64) *U64 {
 	// Handle larger than max
 	x1 := big.Int(*x)
 	y1 := big.Int(*y)
-	z1 := big.Int(*z)
+	z1 := big.Int(*u)
 	z1.Add(&x1, &y1)
-	z.set(z1)
-	return z
+	u.set(z1)
+	return u
 }
 
-func (z *U64) Sub(x *U64, y *U64) *U64 {
+func (u *U64) Sub(x *U64, y *U64) *U64 {
 	// Handle less than zero
 	x1 := big.Int(*x)
 	y1 := big.Int(*y)
-	z1 := big.Int(*z)
+	z1 := big.Int(*u)
 	z1.Sub(&x1, &y1)
-	z.set(z1)
-	return z
+	u.set(z1)
+	return u
 }
 
-func (z *U64) Mul(x *U64, y *U64) *U64 {
+func (u *U64) Mul(x *U64, y *U64) *U64 {
 	x1 := big.Int(*x)
 	y1 := big.Int(*y)
-	z1 := big.Int(*z)
+	z1 := big.Int(*u)
 	z1.Sub(&x1, &y1)
-	z.set(z1)
-	return z
+	u.set(z1)
+	return u
 }
 
-func (z *U64) Idiv(x *U64, y *U64) *U64 {
+func (u *U64) Idiv(x *U64, y *U64) *U64 {
 	x1 := big.Int(*x)
 	y1 := big.Int(*y)
-	z1 := big.Int(*z)
+	z1 := big.Int(*u)
 	z1.Quo(&x1, &y1)
-	z.set(z1)
-	return z
+	u.set(z1)
+	return u
 }
 
-func (z *U64) Mod(x *U64, y *U64) *U64 {
+func (u *U64) Mod(x *U64, y *U64) *U64 {
 	x1 := big.Int(*x)
 	y1 := big.Int(*y)
-	z1 := big.Int(*z)
+	z1 := big.Int(*u)
 	z1.Mod(&x1, &y1)
-	z.set(z1)
-	return z
+	u.set(z1)
+	return u
 }
 
-func (z U64) RlpBytes() []byte {
-	z1 := big.Int(z)
+func (u U64) RlpBytes() []byte {
+	z1 := big.Int(u)
 	x, _ := rlp.EncodeToBytes(z1.Bytes())
 	return x
 }
@@ -188,7 +189,7 @@ func (u *U64) LTE(v *U64) (r int) {
 func (u U64) toJSON() string {
 	u1 := big.Int(u)
 	innerArrOfU := u1.Bytes()
-	var test []byte = make([]byte, 2*len(innerArrOfU))
+	var test = make([]byte, 2*len(innerArrOfU))
 	hex.Encode(test, innerArrOfU[:])
 	return "0x" + string(test)
 }
@@ -204,59 +205,59 @@ func NewU128(v string) U128 {
 	return U128(x)
 }
 
-func (z *U128) set(y big.Int) {
-	*z = U128(y)
+func (u *U128) set(y big.Int) {
+	*u = U128(y)
 }
 
-func (z *U128) Add(x *U128, y *U128) *U128 {
+func (u *U128) Add(x *U128, y *U128) *U128 {
 	// Error larger than max
 	x1 := big.Int(*x)
 	y1 := big.Int(*y)
-	z1 := big.Int(*z)
+	z1 := big.Int(*u)
 	z1.Add(&x1, &y1)
-	z.set(z1)
-	return z
+	u.set(z1)
+	return u
 }
 
-func (z *U128) Sub(x *U128, y *U128) *U128 {
+func (u *U128) Sub(x *U128, y *U128) *U128 {
 	// Error less than zero
 	x1 := big.Int(*x)
 	y1 := big.Int(*y)
-	z1 := big.Int(*z)
+	z1 := big.Int(*u)
 	z1.Sub(&x1, &y1)
-	z.set(z1)
-	return z
+	u.set(z1)
+	return u
 }
 
-func (z *U128) Mul(x *U128, y *U128) *U128 {
+func (u *U128) Mul(x *U128, y *U128) *U128 {
 	x1 := big.Int(*x)
 	y1 := big.Int(*y)
-	z1 := big.Int(*z)
+	z1 := big.Int(*u)
 	z1.Sub(&x1, &y1)
-	z.set(z1)
-	return z
+	u.set(z1)
+	return u
 }
 
-func (z *U128) Idiv(x *U128, y *U128) *U128 {
+func (u *U128) Idiv(x *U128, y *U128) *U128 {
 	x1 := big.Int(*x)
 	y1 := big.Int(*y)
-	z1 := big.Int(*z)
+	z1 := big.Int(*u)
 	z1.Quo(&x1, &y1)
-	z.set(z1)
-	return z
+	u.set(z1)
+	return u
 }
 
-func (z *U128) Mod(x *U128, y *U128) *U128 {
+func (u *U128) Mod(x *U128, y *U128) *U128 {
 	x1 := big.Int(*x)
 	y1 := big.Int(*y)
-	z1 := big.Int(*z)
+	z1 := big.Int(*u)
 	z1.Mod(&x1, &y1)
-	z.set(z1)
-	return z
+	u.set(z1)
+	return u
 }
 
-func (z U128) RlpBytes() []byte {
-	z1 := big.Int(z)
+func (u U128) RlpBytes() []byte {
+	z1 := big.Int(u)
 	x, _ := rlp.EncodeToBytes(z1.Bytes())
 	return x
 }
@@ -371,7 +372,7 @@ func (u *U128) LTE(v *U128) (r int) {
 func (u U128) toJSON() string {
 	u1 := big.Int(u)
 	innerArrOfU := u1.Bytes()
-	var test []byte = make([]byte, 2*len(innerArrOfU))
+	var test = make([]byte, 2*len(innerArrOfU))
 	hex.Encode(test, innerArrOfU[:])
 	return "0x" + string(test)
 }
@@ -387,59 +388,59 @@ func NewU256(v string) U256 {
 	return U256(x)
 }
 
-func (z *U256) set(y big.Int) {
-	*z = U256(y)
+func (u *U256) set(y big.Int) {
+	*u = U256(y)
 }
 
-func (z *U256) Add(x *U256, y *U256) *U256 {
+func (u *U256) Add(x *U256, y *U256) *U256 {
 	// Error larger than max
 	x1 := big.Int(*x)
 	y1 := big.Int(*y)
-	z1 := big.Int(*z)
+	z1 := big.Int(*u)
 	z1.Add(&x1, &y1)
-	z.set(z1)
-	return z
+	u.set(z1)
+	return u
 }
 
-func (z *U256) Sub(x *U256, y *U256) *U256 {
+func (u *U256) Sub(x *U256, y *U256) *U256 {
 	// Error less than zero
 	x1 := big.Int(*x)
 	y1 := big.Int(*y)
-	z1 := big.Int(*z)
+	z1 := big.Int(*u)
 	z1.Sub(&x1, &y1)
-	z.set(z1)
-	return z
+	u.set(z1)
+	return u
 }
 
-func (z *U256) Mul(x *U256, y *U256) *U256 {
+func (u *U256) Mul(x *U256, y *U256) *U256 {
 	x1 := big.Int(*x)
 	y1 := big.Int(*y)
-	z1 := big.Int(*z)
+	z1 := big.Int(*u)
 	z1.Sub(&x1, &y1)
-	z.set(z1)
-	return z
+	u.set(z1)
+	return u
 }
 
-func (z *U256) Idiv(x *U256, y *U256) *U256 {
+func (u *U256) Idiv(x *U256, y *U256) *U256 {
 	x1 := big.Int(*x)
 	y1 := big.Int(*y)
-	z1 := big.Int(*z)
+	z1 := big.Int(*u)
 	z1.Quo(&x1, &y1)
-	z.set(z1)
-	return z
+	u.set(z1)
+	return u
 }
 
-func (z *U256) Mod(x *U256, y *U256) *U256 {
+func (u *U256) Mod(x *U256, y *U256) *U256 {
 	x1 := big.Int(*x)
 	y1 := big.Int(*y)
-	z1 := big.Int(*z)
+	z1 := big.Int(*u)
 	z1.Mod(&x1, &y1)
-	z.set(z1)
-	return z
+	u.set(z1)
+	return u
 }
 
-func (z U256) RlpBytes() []byte {
-	z1 := big.Int(z)
+func (u U256) RlpBytes() []byte {
+	z1 := big.Int(u)
 	x, _ := rlp.EncodeToBytes(z1.Bytes())
 	return x
 }
@@ -554,7 +555,7 @@ func (u *U256) LTE(v *U256) (r int) {
 func (u U256) toJSON() string {
 	u1 := big.Int(u)
 	innerArrOfU := u1.Bytes()
-	var test []byte = make([]byte, 2*len(innerArrOfU))
+	var test = make([]byte, 2*len(innerArrOfU))
 	hex.Encode(test, innerArrOfU[:])
 	return "0x" + string(test)
 }

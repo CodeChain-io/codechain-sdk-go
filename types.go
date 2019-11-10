b0a72443e673bfc1bb4ecbab1f18cba5c83baa17
Seongjae Choi <sjchoi@kodebox.io>
2019-11-11 08:15:39 +0900

    Add transaction types

--- types.go
+++ types.go
@@ -4,7 +4,84 @@ type UnsignedTransaction struct {
 	fee       int
 	networkID string
 	seq       int
-	action    interface{}
+	action    Action
+}
+
+type AssetScheme interface{}
+type Asset interface{}
+type CommonParams interface{}
+
+type Text struct {
+	content   string `json:"content"`
+	certifier string `json:"certifier"`
+}
+
+type Action interface {
+	getType() string
+}
+
+type MintAsset struct {
+	Type                string      `json:"type"`
+	NetworkID           string      `json:"network_id"`
+	ShardID             int         `json:"shard_id"`
+	Metadata            string      `json:"metadata"`
+	Approver            string      `json:"appprover,omitempty"`
+	Registrar           string      `json:"registrar,omitempty"`
+	AllowedScriptHashes []string    `json:"allowed_script_hashes"`
+	Output              interface{} `json:"output"`
+	Approvals           []string    `json:"approvals"`
+	Tracker             string      `json:"tracker"`
+}
+
+type TransferAsset struct {
+	Type       string        `json:"type"`
+	NetworkID  string        `json:"network_id"`
+	Burns      []interface{} `json:"burns"`
+	Inputs     []interface{} `json:"inputs"`
+	Outputs    []interface{} `json:"outputs"`
+	Orders     []interface{} `json:"orders"`
+	Metadata   string        `json:"metadata"`
+	Approvals  []string      `json:"approvals"`
+	Tracker    string        `json:"tracker"`
+	Expiration int           `json:"expiration,omitempty"`
+}
+
+type ChangeAssetScheme struct {
+	Type string `json:"type"`
+}
+
+type IncreaseAssetSupply struct {
+	Type string `json:"type"`
+}
+
+type UnwrapCCC struct {
+	Type string `json:"type"`
+}
+
+type WrapCCC struct {
+	Type string `json:"type"`
+}
+
+type Pay struct {
+	Type string `json:"type"`
+}
+
+type SetRegularKey struct {
+	Type string `json:"type"`
+}
+
+type Store struct {
+	Type string `json:"type"`
+}
+
+type Remove struct {
+	Type string `json:"type"`
+}
+
+type Custom struct {
+	Type      string `json:"type"`
+	NetworkID string `json:"network_id"`
+	HandlerID string `json:"handler_id"`
 }
 
 type Transaction struct {
@@ -17,7 +94,7 @@ type Transaction struct {
 	hash             string
 	networkID        string
 	sig              string
-	action           string
+	action           Action
 } // TODO Handle action details
 
 type Block struct {
@@ -34,7 +111,46 @@ type Block struct {
 	TransactionsRoot string        `json:"transactionsRoot"`
 }
 
-type Text struct {
-	content   string
-	certifier string
+func (t MintAsset) getType() string {
+	return "mintAsset"
+}
+
+func (t TransferAsset) getType() string {
+	return "transferAsset"
+}
+
+func (t ChangeAssetScheme) getType() string {
+	return "changeAssetScheme"
+}
+
+func (t IncreaseAssetSupply) getType() string {
+	return "increaseAssetSupply"
+}
+
+func (t UnwrapCCC) getType() string {
+	return "unwrapCCC"
+}
+
+func (t WrapCCC) getType() string {
+	return "wrapCCC"
+}
+
+func (t Pay) getType() string {
+	return "pay"
+}
+
+func (t SetRegularKey) getType() string {
+	return "setRegularKey"
+}
+
+func (t Store) getType() string {
+	return "store"
+}
+
+func (t Remove) getType() string {
+	return "remove"
+}
+
+func (t Custom) getType() string {
+	return "custom"
 }

07a63cd48b6e5b28600682b60ff7b5077b7bb4fd
Seongjae Choi <sjchoi@kodebox.io>
2019-11-11 08:13:21 +0900

    Handle errors in chain rpc testing

--- chain_test.go
+++ chain_test.go
@@ -7,6 +7,9 @@ import (
 
 func TestChain(t *testing.T) {
 	chain := NewRPC("https://corgi-rpc.codechain.io/").chain
-	res := chain.GetBlockByNumber(5)
+	res, err := chain.GetBlockByNumber(5)
+	if err != nil {
+		t.Fatal("TestChain failed")
+	}
 	fmt.Println(res.Hash)
 }

a98d2ee53db9e7c7e9a3ec1f3e829ace31104c50
Seongjae Choi <sjchoi@kodebox.io>
2019-11-28 11:29:24 +0900

    Add transaction objects to sdk

---
+++ core/transaction/signedTransaction.go
@@ -0,0 +1,78 @@
+package transaction
+
+import (
+	"github.com/CodeChain-io/codechain-rpc-go/crypto"
+	"github.com/CodeChain-io/codechain-rpc-go/primitives"
+	// "github.com/ethereum/go-ethereum/rlp"
+)
+
+type SignedTransactionJSON struct {
+	BlockNumber      int    `json:"blockNumber,omitempty"`
+	BlockHash        string `json:"blockHash,omitempty"`
+	TransactionIndex int    `json:"transactionIndex,omitempty"`
+	Sig              string `json:"sig"`
+	Hash             string `json:"hash"`
+}
+
+type SignedTransaction struct {
+	Unsigned         transaction
+	BlockNumber      *int
+	BlockHash        *primitives.H256
+	TransactionIndex *int
+	signature        string
+}
+
+func NewSignedTransaction(
+	unsigned transaction,
+	signature string,
+	blockNumber *int,
+	blockHash *primitives.H256,
+	transactionIndex *int) SignedTransaction {
+	return SignedTransaction{unsigned, blockNumber, blockHash, transactionIndex, signature}
+}
+
+func (t SignedTransaction) Signature() string {
+	return t.signature
+}
+
+/*func (t SignedTransaction) ToEncodeObject() []interface{} {
+	return []interface{}{t.Unsigned.ToEncodeObject(), t.Signature()}
+}*/
+
+/*func (t SignedTransaction) RlpBytes() []byte {
+	x, _ := rlp.EncodeToBytes(t.ToEncodeObject())
+	return x
+}*/
+
+func (t SignedTransaction) Hash() primitives.H256 {
+	hash, _ := crypto.Blake256([]byte{0}) //t.RlpBytes())
+
+	var value [32]byte
+	copy(value[:], hash[:32])
+	return primitives.H256(value) // byte
+}
+
+func (t SignedTransaction) GetSignerAccountID() primitives.H160 {
+	pubKey := crypto.RecoverEcdsa(t.Unsigned.UnsignedHash().Bytes(), []byte(t.signature))
+	id, _ := crypto.Blake160(pubKey)
+	return primitives.NewH160(id)
+}
+
+func (t SignedTransaction) GetSignerAddress(networkID string) primitives.PlatformAddress {
+	a, _ := primitives.PlatformAddressFromAccountID(t.GetSignerAccountID(), networkID)
+	return a
+}
+
+func (t SignedTransaction) GetSignerPublic() primitives.H512 {
+	a := crypto.RecoverEcdsa(t.Unsigned.UnsignedHash().Bytes(), []byte(t.signature))
+	return primitives.NewH512(a)
+}
+
+func (t SignedTransaction) ToJSON() SignedTransactionJSON {
+	return SignedTransactionJSON{
+		*t.BlockNumber,
+		t.BlockHash.ToString(),
+		*t.TransactionIndex,
+		t.signature,
+		t.Hash().ToJSON()}
+}

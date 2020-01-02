c5e7a814ed8f2b3f552b38e61d7d0d318faff5e4
Seongjae Choi <sjchoi@kodebox.io>
2020-01-02 12:17:32 +0900

    Implement createAssetAddress and createPlatformAddress

--- key/key_test.go
+++ key/key_test.go
@@ -8,17 +8,18 @@ import (
 )
 
 func TestEcdsa(t *testing.T) {
-	key, err := GenerateKey()
+	key, err := GenerateEcdsa()
 	if err != nil {
-		t.Fatal("Fail to generate private key")
+		t.Fatal("Fail to generate ecdsa")
 	}
+	privKey := key.GetPrivateKey()
 
 	msg, err := hex.DecodeString("aabbccdd0011223344aabbccdd0011223344aabbccdd0011223344aabbccdd00")
 	if err != nil {
 		t.Fatal("Fail to decode msg")
 	}
 
-	signature := crypto.SignEcdsa(msg, key)
+	signature := crypto.SignEcdsa(msg, privKey)
 	if signature == nil {
 		t.Fatal("Signature is empty")
 	}

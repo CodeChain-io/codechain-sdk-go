9357216da535607d54af00f9abdf26e7feebe054
Seongjae Choi <sjchoi@kodebox.io>
2020-01-10 19:17:26 +0900

    Implement createUnlockScript in P2PKH

--- key/P2PKH.go
+++ key/P2PKH.go
@@ -1,6 +1,7 @@
 package key
 
 import (
+	"github.com/CodeChain-io/codechain-sdk-go/crypto"
 	"github.com/CodeChain-io/codechain-sdk-go/primitives"
 	"github.com/CodeChain-io/codechain-sdk-go/vm"
 )
@@ -14,4 +15,24 @@ func GetP2PKHLockScriptHash() primitives.H160 {
 	return hash
 }
 
-// TODO createAddress, createUnlockScript
+func CreateP2PKHUnlockScript(pubkey []byte, privkey []byte, txHash primitives.H256) []byte {
+
+	encodedTag := []byte{3}
+
+	signature := crypto.SignEcdsa(txHash.Bytes(), privkey)
+	pushb := byte(vm.PUSHB)
+
+	result := []byte{pushb, 65}
+
+	result = append(result, signature...)
+	result = append(result, pushb)
+	result = append(result, byte(len(encodedTag)))
+	result = append(result, encodedTag...)
+	result = append(result, pushb)
+	result = append(result, 64)
+	result = append(result, pubkey...)
+
+	return result
+}
+
+// TODO createAddress
